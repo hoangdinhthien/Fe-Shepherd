672ed675d9fda23f7c7546687e89c45700cc71a4
n-d-h <hoangdtse161991@fpt.edu.vn>
2024-11-07 15:34:24 +0700

    fix

---
+++ src/components/event/EventHeader.jsx
@@ -0,0 +1,45 @@
+import { Divider, Pagination, Select, Tabs } from "antd";
+import TabPane from "antd/es/tabs/TabPane";
+
+const filters = ['All', 'This month', 'Next month'];
+
+const EventHeader = ({ setActiveTab, filter, setFilter, currentPage, total, setCurrentPage }) => {
+    return (
+        <div className='relative flex justify-between w-full items-center mb-0 mx-4'>
+        <Tabs
+          defaultActiveKey="events"
+          onChange={(key) => setActiveTab(key)}
+          className="w-full border-b-1 border-[#D9D9D9]"
+        >
+          <TabPane tab="List Events" key="events" />
+          <TabPane disabled tab={
+            <span>
+              <Divider type="vertical" className="bg-black h-[20px]" />
+            </span>
+          } />
+          <TabPane tab="List Activities" key="activities" />
+        </Tabs>
+
+
+        <div className='absolute right-12 top-0 flex items-center gap-8'>
+          <Select
+            value={`Sort by: ${filters[filter]}`}
+            onChange={(value) => { setFilter(filters.findIndex((item) => item === value)); setCurrentPage(1) }}
+            size="large"
+            className="w-[fit] bg-transparent font-bold"
+            placeholder="Sort by: All"
+            variant='filled'
+          >
+            <Option value={filters[0]}>All</Option>
+            <Option value={filters[1]}>This Month</Option>
+            <Option value={filters[2]}>Next Month</Option>
+          </Select>
+
+          <Pagination current={currentPage} total={total} onChange={(pageNumber) => setCurrentPage(pageNumber)}
+          />
+        </div>
+      </div>
+    )
+}
+
+export default EventHeader;
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import { Modal, Select, Tabs, Card, Typography, Divider, DatePicker, message, Spin } from 'antd';
-import { CalendarFilled, CalendarOutlined, FileTextFilled, FileTextOutlined } from '@ant-design/icons';
-import EventAPI from '../apis/event_api';
-import ActivityAPI from '../apis/activity/activity_api';
-import moment from 'moment';
-import ALT from '../assets/welcome-img.png';
-import { useSelector } from 'react-redux';
+import { CalendarFilled, FileTextFilled } from '@ant-design/icons';
 import { Spinner } from '@material-tailwind/react';
+import { Card, message, Select, Tabs, Typography } from 'antd';
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import ActivityAPI from '../apis/activity/activity_api';
+import EventAPI from '../apis/event_api';
+import ALT from '../assets/welcome-img.png';
+import CreateActivityModal from '../components/event/CreateActivityModal';
+import EventHeader from '../components/event/EventHeader';
 
-const { TabPane } = Tabs;
-const { Option } = Select;
 const { Title, Paragraph } = Typography;
 
 export default function EventActivityPage() {
@@ -19,14 +19,15 @@ export default function EventActivityPage() {
   const [filteredActivities, setFilteredActivities] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [filter, setFilter] = useState('All');
+  const [filter, setFilter] = useState(0);
   const [activeTab, setActiveTab] = useState('events');
   const [showCreateActivity, setShowCreateActivity] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
   const COUNCIL = import.meta.env.VITE_ROLE_COUNCIL;
   const { currentUser } = useSelector((state) => state.user);
-
   const groupIds =
     currentUser.listGroupRole
       .filter(
@@ -92,14 +93,15 @@ export default function EventActivityPage() {
     } else {
       fetchActivities();
     }
-  }, [activeTab]);
+  }, [activeTab, currentPage, filter]);
 
   const fetchEvents = async () => {
     try {
       setLoading(true);
-      const response = await EventAPI.getEvents();
+      const response = await EventAPI.getAll({ PageNumber: currentPage, PageSize: 8, FilterBy: filter });
       setEvents(response.result);
       setFilteredEvents(response.result);
+      setTotal(response.pagination.totalCount);
     } catch (error) {
       console.error('Failed to fetch events:', error);
     } finally {
@@ -110,9 +112,10 @@ export default function EventActivityPage() {
   const fetchActivities = async () => {
     try {
       setLoading(true);
-      const response = await ActivityAPI.getAll();
+      const response = await ActivityAPI.getAll({ PageNumber: currentPage, PageSize: 8 });
       setActivities(response.result);
       setFilteredActivities(response.result);
+      setTotal(response.pagination.totalCount);
     } catch (error) {
       console.error('Failed to fetch activities:', error);
     } finally {
@@ -120,46 +123,19 @@ export default function EventActivityPage() {
     }
   };
 
-  const handleFilterChange = (value) => {
-    setFilter(value);
-    if (value === 'all') {
-      setFilteredEvents(events);
-      setFilteredActivities(activities);
-    } else if (value === 'this month') {
-      const currentMonth = moment().month();
-      setFilteredEvents(
-        events.filter(
-          (event) => moment(event.fromDate).month() === currentMonth
-        )
-      );
-      setFilteredActivities(
-        activities.filter(
-          (activity) => moment(activity.startDate).month() === currentMonth
-        )
-      );
-    } else if (value === 'next month') {
-      const nextMonth = moment().add(1, 'month').month();
-      setFilteredEvents(
-        events.filter((event) => moment(event.fromDate).month() === nextMonth)
-      );
-      setFilteredActivities(
-        activities.filter(
-          (activity) => moment(activity.startDate).month() === nextMonth
-        )
-      );
-    }
-  };
-
   const showModal = (item) => {
     if (activeTab === 'events') {
-      setFormData({
-        activityName: "",
-        description: "",
-        startTime: null,
-        endTime: null,
-        groups: groups,
-        eventID: item.id,
-      });
+      setIsUpdate(false);
+      if (!isUpdate) {
+        setFormData({
+          activityName: "",
+          description: "",
+          startTime: null,
+          endTime: null,
+          groups: groups,
+          eventID: item.id,
+        });
+      }
     };
     setSelectedItem(item);
     setIsModalVisible(true);
@@ -171,194 +147,77 @@ export default function EventActivityPage() {
   };
 
   return (
-    <div className='p-6 shadow-lg bg-[#d2d2d2] rounded-3xl'>
-      <div className='relative flex justify-between items-center mb-4 mx-4'>
-        <Tabs
-          defaultActiveKey="events"
-          onChange={(key) => setActiveTab(key)}
-          className="w-full border-b-1 border-[#D9D9D9]"
-        >
-          <TabPane tab="List Events" key="events" />
-          <TabPane disabled tab={
-            <span>
-              <Divider type="vertical" className="bg-black h-[20px]" />
-            </span>
-          } />
-          <TabPane tab="List Activities" key="activities" />
-        </Tabs>
+    <div className='px-6 py-2 w-full shadow-lg bg-[#e3e3e3] rounded-3xl'>
+      <EventHeader
+        setActiveTab={setActiveTab}
+        filter={filter}
+        setFilter={setFilter}
+        currentPage={currentPage}
+        total={total}
+        setCurrentPage={setCurrentPage} />
 
-        <Select
-          value={`Sort by: ${filter}`}
-          onChange={handleFilterChange}
-          size="large"
-          className="absolute right-0 top-0 w-[fit] bg-transparent font-bold"
-          placeholder="Sort by: All"
-          variant='borderless'
-        >
-          <Option value="All">All</Option>
-          <Option value="This month">This Month</Option>
-          <Option value="Next month">Next Month</Option>
-        </Select>
+      <div className={`grid w-full grid-cols-4 px-4 pb-3 gap-${loading ? '0' : '4'}`}>
+        {loading ? (
+          <div className="col-span-4 h-[75vh] flex justify-center items-center">
+            <Spinner className="h-[40px]" spinning="true" />
+          </div>
+        ) : (
+          (activeTab === 'events' ? filteredEvents : filteredActivities)?.map((item) => (
+            <Card
+              key={item.id}
+              hoverable
+              className="rounded-2xl overflow-hidden shadow-md h-[345px]"
+              onClick={() => {
+                showModal(item);
+                setShowCreateActivity(false);
+              }}
+              cover={
+                <div className={`${activeTab === 'activities' && 'relative'}`}>
+                  <img alt={item.eventName || item.activityName} src={ALT} />
+                  {activeTab === 'activities' && (
+                    <div className="absolute bottom-2 left-2 rounded-xl bg-[#71BE63] max-w-[300px] shadow-lg overflow-ellipsis text-white font-bold p-2 px-3 text-sm">
+                      {item.event.eventName}
+                    </div>
+                  )}
+                </div>
+              }
+            >
+              <div className='flex gap-4'>
+                <Title ellipsis={{ rows: 2 }} level={4}>{item.eventName || item.activityName}</Title>
+
+              </div>
+              <div className="flex items-center w-full text-gray-500 mb-1">
+                <CalendarFilled className="mr-2" />
+                <span>{moment(item.fromDate || item.startDate).format('DD/MM/YYYY')}</span>
+                <span className="mx-5">to</span>
+                <span>{moment(item.toDate || item.endDate).format('DD/MM/YYYY')}</span>
+              </div>
+              <div className="flex items-start justify-start w-full text-gray-500">
+                <FileTextFilled className="mr-2 mt-1" />
+                <Paragraph ellipsis={{ rows: 1 }}>{item.description ?? 'N/A'}</Paragraph>
+              </div>
+            </Card>
+          ))
+        )}
       </div>
 
-      {loading && <div className='h-[75vh] flex justify-center items-center w-full'><Spinner className='w-full h-[40px]' spinning={loading} /></div>}
-      {!loading && (
-        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8'>
-          {(activeTab === 'events' ? filteredEvents : filteredActivities)?.map(
-            (item) => (
-              <Card
-                key={item.id}
-                hoverable
-                className='rounded-2xl overflow-hidden shadow-md'
-                onClick={() => { showModal(item); setShowCreateActivity(false) }}
-                cover={
-                  <div className={`${activeTab === 'activities' && 'relative'}`} >
-                    <img
-                      alt={item.eventName || item.activityName}
-                      src={ALT}
-                    />
-                    {activeTab === 'activities' && <div className='absolute bottom-2 left-2 rounded-xl bg-[#71BE63] mx-w-[300px] shadow-lg overflow-ellipsis text-white font-bold p-2 px-3 text-sm'>{item.event.eventName}</div>}
-                  </div>
-                }
-              >
-                <Title level={4}>{item.eventName || item.activityName}</Title>
-                <div className='flex items-center w-full text-gray-500 mb-1'>
-                  <CalendarFilled className='mr-2' />
-                  <span>
-                    {moment(item.fromDate || item.startDate).format(
-                      'DD/MM/YYYY'
-                    )}
-                  </span>
-                  <span className='mx-5'>to</span>
-                  <span>
-                    {moment(item.toDate || item.endDate).format('DD/MM/YYYY')}
-                  </span>
-                </div>
-                <div className='flex items-start justify-start w-full text-gray-500'>
-                  <FileTextFilled className='mr-2 mt-1' />
-                  <Paragraph ellipsis={{ rows: 3 }}>{item.description ?? 'N/A'}</Paragraph>
-                </div>
-              </Card>
-            )
-          )}
-        </div>
-      )}
-
-      <Modal
+      <CreateActivityModal
+        isUpdate={isUpdate}
+        setIsUpdate={setIsUpdate}
+        groupIds={groupIds}
+        image={ALT}
         loading={loading}
-        open={isModalVisible}
-        onCancel={handleModalClose}
-        footer={null}
-        width={800}
-        centered
-      >
-        {selectedItem && (
-          <div className='p-4'>
-            <img
-              alt={selectedItem.eventName || selectedItem.activityName}
-              src={ALT}
-              className='w-full h-48 object-fill rounded-xl mb-4'
-            />
-            <Title level={3}>
-              {selectedItem.eventName || selectedItem.activityName}
-            </Title>
-            <div className='flex items-center text-gray-500 mb-2'>
-              <CalendarOutlined className='mr-2' />
-              <span>
-                {moment(selectedItem.fromDate || selectedItem.startDate).format(
-                  'DD MMM YYYY'
-                )}
-              </span>
-              <span className='mx-5'>to</span>
-              <span>
-                {moment(selectedItem.toDate || selectedItem.endDate).format(
-                  'DD MMM YYYY'
-                )}
-              </span>
-            </div>
-            <Paragraph>{selectedItem.description}</Paragraph>
-            <div className='flex w-full justify-between items-center'>
-              <a
-                href='#'
-                className='text-blue-500'
-              >
-                Go to the{' '}
-                {activeTab === 'events'
-                  ? 'Activities of this Event'
-                  : 'Tasks of this Activity'}
-              </a>
-              {activeTab === 'events' && !showCreateActivity && groupIds.length > 0 && (
-                <button
-                  onClick={() => setShowCreateActivity(true)}
-                  className='bg-black hover:opacity-65 text-white px-4 py-2 rounded-xl'
-                >
-                  Create Activities
-                </button>)}
-            </div>
-
-
-            {showCreateActivity && (
-              <div className='flex flex-col gap-2 w-full mt-4'>
-                <p className='font-semibold text-lg text-[#D9D9D9] text-center border-t-2 pt-2 mx-20 border-dashed uppercase'>Create Activities</p>
-                <form className='flex flex-col w-full'>
-                  <p className='font-semibold m-2'>Activity Name</p>
-                  <input
-                    required
-                    name='activityName'
-                    value={formData.activityName}
-                    onChange={handleChange}
-                    type='text'
-                    placeholder='Activity Name'
-                    className='border p-2 rounded-lg mb-4'
-                  />
-                  <p className='font-semibold m-2'>Description</p>
-                  <textarea
-                    required
-                    name='description'
-                    value={formData.description}
-                    onChange={handleChange}
-                    placeholder='Description'
-                    className='border p-2 rounded-lg mb-4'
-                  />
-                  <p className='font-semibold m-2'>Time</p>
-                  <DatePicker.RangePicker
-                    required
-                    picker="time"
-                    format="HH:mm:ss"
-                    value={[
-                      formData.startTime ? moment(formData.startTime, 'HH:mm:ss') : null,
-                      formData.endTime ? moment(formData.endTime, 'HH:mm:ss') : null
-                    ]}
-                    onChange={(date) =>
-                      setFormData((prev) => ({
-                        ...prev,
-                        startTime: date && date[0] ? date[0].format('HH:mm:ss') : null,
-                        endTime: date && date[1] ? date[1].format('HH:mm:ss') : null,
-                      }))
-                    }
-                    placeholder={['Start Time', 'End Time']}
-                    className="border border-[#EEE] p-2 rounded-lg w-full text-sm font-semibold"
-                  />
-                  <div className='flex flex-row justify-end items-end w-full gap-4 mt-4'>
-                    <button
-                      onClick={() => setShowCreateActivity(false)}
-                      className='bg-red-500 hover:opacity-65 text-white px-4 py-2 rounded-xl'
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      onClick={onSubmit}
-                      className='bg-black hover:opacity-65 text-white px-4 py-2 rounded-xl'
-                    >
-                      Create Activities
-                    </button>
-                  </div>
-                </form>
-              </div>
-            )}
-          </div>
-        )}
-      </Modal>
+        isModalVisible={isModalVisible}
+        handleModalClose={handleModalClose}
+        selectedItem={selectedItem}
+        showCreateActivity={showCreateActivity}
+        setShowCreateActivity={setShowCreateActivity}
+        activeTab={activeTab}
+        formData={formData}
+        setFormData={setFormData}
+        handleChange={handleChange}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 }
