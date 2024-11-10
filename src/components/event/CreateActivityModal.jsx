3d7be6b9e292d2709b1bc67a87cf131e17f7584c
n-d-h <hoangdtse161991@fpt.edu.vn>
2024-11-10 11:00:12 +0700

    fix

--- src/components/event/CreateActivityModal.jsx
+++ src/components/event/CreateActivityModal.jsx
@@ -1,4 +1,4 @@
-import { CalendarOutlined, EditOutlined } from '@ant-design/icons';
+import { CalendarOutlined, CheckOutlined, CloseOutlined, EditOutlined } from '@ant-design/icons';
 import { DatePicker, Modal, Typography } from "antd";
 import moment from "moment";
 const { Title, Paragraph } = Typography;
@@ -44,8 +44,26 @@ const CreateActivityModal = ({
                     />
                     <Title level={3}>
                         {selectedItem.eventName || selectedItem.activityName}
-                        <EditOutlined onClick={onUpdateClicked} className="text-blue-500 ml-4 cursor-pointer p-2 rounded-xl hover:bg-blue-gray-300 hover:text-white" />
+                        {!isUpdate && (
+                            <EditOutlined
+                                onClick={onUpdateClicked}
+                                className="text-white ml-4 cursor-pointer p-2 rounded-xl bg-blue-500 hover:bg-blue-600 hover:text-white transition-colors duration-300 ease-in-out"
+                            />
+                        )}
+                        {isUpdate && (
+                            <CloseOutlined
+                                onClick={() => { setIsUpdate(false); setShowCreateActivity(false) }}
+                                className="text-white ml-4 cursor-pointer p-2 rounded-xl bg-red-500 hover:bg-red-600 hover:text-white transition-colors duration-300 ease-in-out"
+                            />
+                        )}
+                        {isUpdate && (
+                            <CheckOutlined
+                                onClick={() => { setIsUpdate(false); setShowCreateActivity(false) }}
+                                className="text-white ml-4 cursor-pointer p-2 rounded-xl bg-green-500 hover:bg-green-600 hover:text-white transition-colors duration-300 ease-in-out"
+                            />
+                        )}
                     </Title>
+
                     <div className='flex items-center text-gray-500 mb-2'>
                         <CalendarOutlined className='mr-2' />
                         <span>
@@ -79,66 +97,6 @@ const CreateActivityModal = ({
                                 Create Activities
                             </button>)}
                     </div>
-
-                    {showCreateActivity && isUpdate && (
-                        <div className='flex flex-col gap-2 w-full mt-4'>
-                            <p className='font-semibold text-lg text-[#D9D9D9] text-center border-t-2 pt-2 mx-20 border-dashed uppercase'>Update Event</p>
-                            <form className='flex flex-col w-full'>
-                                <p className='font-semibold m-2'>Event Name</p>
-                                <input
-                                    required
-                                    name='eventName'
-                                    // value={formData.eventName}
-                                    // onChange={handleChange}
-                                    type='text'
-                                    placeholder='Event Name'
-                                    className='border p-2 rounded-lg mb-4'
-                                />
-                                <p className='font-semibold m-2'>Description</p>
-                                <textarea
-                                    required
-                                    name='description'
-                                    // value={formData.description}
-                                    // onChange={handleChange}
-                                    placeholder='Description'
-                                    className='border p-2 rounded-lg mb-4'
-                                />
-                                {/* <p className='font-semibold m-2'>Time</p> */}
-                                {/* <DatePicker.RangePicker
-                                    required
-                                    picker="time"
-                                    format="HH:mm:ss"
-                                    value={[
-                                        formData.startTime ? moment(formData.startTime, 'HH:mm:ss') : null,
-                                        formData.endTime ? moment(formData.endTime, 'HH:mm:ss') : null
-                                    ]}
-                                    onChange={(date) =>
-                                        setFormData((prev) => ({
-                                            ...prev,
-                                            startTime: date && date[0] ? date[0].format('HH:mm:ss') : null,
-                                            endTime: date && date[1] ? date[1].format('HH:mm:ss') : null,
-                                        }))
-                                    }
-                                    placeholder={['Start Time', 'End Time']}
-                                    className="border border-[#EEE] p-2 rounded-lg w-full text-sm font-semibold"
-                                /> */}
-                                <div className='flex flex-row justify-end items-end w-full gap-4 mt-4'>
-                                    <button
-                                        onClick={() => {setShowCreateActivity(false); setIsUpdate(false)}}
-                                        className='bg-red-500 hover:opacity-65 text-white px-4 py-2 rounded-xl'
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        onClick={onSubmit}
-                                        className='bg-black hover:opacity-65 text-white px-4 py-2 rounded-xl'
-                                    >
-                                        Update Event
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    )}
 
 
                     {showCreateActivity && !isUpdate && (
--- src/components/event/EventHeader.jsx
+++ src/components/event/EventHeader.jsx
@@ -4,42 +4,48 @@ import TabPane from "antd/es/tabs/TabPane";
 const filters = ['All', 'This month', 'Next month'];
 
 const EventHeader = ({ setActiveTab, filter, setFilter, currentPage, total, setCurrentPage }) => {
-    return (
-        <div className='relative flex justify-between w-full items-center mb-0 mx-4'>
-        <Tabs
-          defaultActiveKey="events"
-          onChange={(key) => setActiveTab(key)}
-          className="w-full border-b-1 border-[#D9D9D9]"
+  return (
+    <div className='relative flex justify-between w-full items-center mb-0 mx-4'>
+      <Tabs
+        defaultActiveKey="events"
+        onChange={(key) => { setActiveTab(key); setCurrentPage(1) }}
+        className="border-b-1 border-[#D9D9D9]"
+      >
+        <TabPane
+          tab={<span className="font-bold">List Events</span>}
+          key="events"
+        />
+        <TabPane disabled tab={
+          <span>
+            <Divider type="vertical" className="bg-black h-[20px]" />
+          </span>
+        } />
+        <TabPane
+          tab={<span className="font-bold">List Activities</span>}
+          key="activities"
+        />
+      </Tabs>
+
+
+      <div className='absolute right-12 top-0 flex items-center gap-8'>
+        <Select
+          value={`Sort by: ${filters[filter]}`}
+          onChange={(value) => { setFilter(filters.findIndex((item) => item === value)); setCurrentPage(1) }}
+          size="large"
+          className="w-[fit] bg-transparent font-bold"
+          placeholder="Sort by: All"
+          variant='filled'
         >
-          <TabPane tab="List Events" key="events" />
-          <TabPane disabled tab={
-            <span>
-              <Divider type="vertical" className="bg-black h-[20px]" />
-            </span>
-          } />
-          <TabPane tab="List Activities" key="activities" />
-        </Tabs>
+          <Option value={filters[0]}>All</Option>
+          <Option value={filters[1]}>This Month</Option>
+          <Option value={filters[2]}>Next Month</Option>
+        </Select>
 
-
-        <div className='absolute right-12 top-0 flex items-center gap-8'>
-          <Select
-            value={`Sort by: ${filters[filter]}`}
-            onChange={(value) => { setFilter(filters.findIndex((item) => item === value)); setCurrentPage(1) }}
-            size="large"
-            className="w-[fit] bg-transparent font-bold"
-            placeholder="Sort by: All"
-            variant='filled'
-          >
-            <Option value={filters[0]}>All</Option>
-            <Option value={filters[1]}>This Month</Option>
-            <Option value={filters[2]}>Next Month</Option>
-          </Select>
-
-          <Pagination current={currentPage} total={total} onChange={(pageNumber) => setCurrentPage(pageNumber)}
-          />
-        </div>
+        <Pagination current={currentPage} total={total} onChange={(pageNumber) => setCurrentPage(pageNumber)}
+        />
       </div>
-    )
+    </div>
+  )
 }
 
 export default EventHeader;
